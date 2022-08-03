410fb2cbf501d0fd13b2dc65db7ee0a5023cdd7a
ElPatatone <rahman.hm2002@gmail.com>
2022-08-03 14:31:08 +0100

    found a working cmp config file and added it

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -52,6 +52,17 @@ return packer.startup(function(use)
   --colorschemes
   use "ellisonleao/gruvbox.nvim"
   
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
   --markdown
   use({
     "iamcco/markdown-preview.nvim",
