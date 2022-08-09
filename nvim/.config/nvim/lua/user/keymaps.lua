2164010fa664fb0171d6f7d5e2d416144f6feaa6
ElPatatone <rahman.hm2002@gmail.com>
2022-08-09 17:11:05 +0100

    feat: add custom keybinds for bufferline

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -36,7 +36,7 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
+keymap("n", "<S-d>", ":bdelete<CR>", opts)
 -- Insert --
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
