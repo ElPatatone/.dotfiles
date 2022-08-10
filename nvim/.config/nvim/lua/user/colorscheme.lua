1dd403a0c91e5ee767a47a7c178102c9983ed7e7
ElPatatone <rahman.hm2002@gmail.com>
2022-08-10 23:44:13 +0100

    feat: changed colorscheme settings

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,8 +1,11 @@
 local colorscheme = "moonfly"
 
+vim.g.moonflyItalics = false
+vim.g.moonflyCursorColor = true
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
+
