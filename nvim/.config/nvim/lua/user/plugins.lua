0a2d1cf305ae22a8a67b482a03a4147c562f2679
ElPatatone <rahman.hm2002@gmail.com>
2022-09-02 18:20:39 +0100

    fix: removed buggy code for the plugin setup

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -109,8 +109,7 @@ return packer.startup(function(use)
   use "lewis6991/gitsigns.nvim" 
 
   --organisation
-  use "vimwiki/vimwiki"
-
+  use { "vimwiki/vimwiki", branch = "dev" }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
