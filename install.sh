e5b6b5042bcfc07f18ffd0c5c4212202e11b6d69
ElPatatone <rahman.hm2002@gmail.com>
2022-12-25 18:39:30 +0000

    fix: remove wrong files

--- install.sh
+++ install.sh
@@ -1,20 +1,3 @@
-# install nix
-curl -L https://nixos.org/nix/install | sh
-
-# source nix
-. ~/.nix-profile/etc/profile.d/nix.sh
-
-# install packages
-nix-env -iA \
-	nixpkgs.zsh \
-	nixpkgs.git \
-	nixpkgs.tmux \
-	nixpkgs.stow \
-	nixpkgs.fzf \
-	nixpkgs.bat \
-	nixpkgs.unzip \
-    nixpkgs.gcc \
-
 # stow
 stow zsh
 stow nvim
