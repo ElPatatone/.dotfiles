3633db817ad88d31303a983aa2130918d11594a7
ElPatatone <rahman.hm2002@gmail.com>
2022-12-25 18:26:12 +0000

    fix: new plugins

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -69,44 +69,55 @@ return packer.startup(function(use)
   use "numToStr/Comment.nvim"
 
   -- telescope
-  use "nvim-telescope/telescope.nvim"
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
   use "nvim-telescope/telescope-media-files.nvim"
 
   use "kyazdani42/nvim-web-devicons"
 
   -- treesitter
-  use "nvim-treesitter/nvim-treesitter"
+  use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
   use "p00f/nvim-ts-rainbow"
   use "JoosepAlviste/nvim-ts-context-commentstring"
+
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
 
   --colorschemes
   use "ellisonleao/gruvbox.nvim"
   use "bluz71/vim-moonfly-colors"
   use "bignimbus/pop-punk.vim"
 
-  -- LSP
-  use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/mason.nvim"
-  use "williamboman/mason-lspconfig.nvim"
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-  use "ray-x/lsp_signature.nvim"
-  use "SmiteshP/nvim-navic"
-  use "RRethy/vim-illuminate"
-  use "j-hui/fidget.nvim"
-  use "lvimuser/lsp-inlayhints.nvim"
 
-  -- Completion
-  use "christianchiarulli/nvim-cmp"
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
 
-  -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+ -- lsp 
+ use {
+      'VonHeikemen/lsp-zero.nvim',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
+
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-buffer'},
+          {'hrsh7th/cmp-path'},
+          {'saadparwaiz1/cmp_luasnip'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'hrsh7th/cmp-nvim-lua'},
+
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},
+          {'rafamadriz/friendly-snippets'},
+      }
+  } 
+  
 
   --markdown
   use({
