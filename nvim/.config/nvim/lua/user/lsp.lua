086d5f79b26737c0dcf238ff38a983b99232f879
ElPatatone <rahman.hm2002@gmail.com>
2022-12-28 18:15:46 +0000

    feat: add set up for clangd

--- nvim/.config/nvim/lua/user/lsp.lua
+++ nvim/.config/nvim/lua/user/lsp.lua
@@ -23,6 +23,27 @@ lsp.configure('sumneko_lua', {
 })
 
 
+
+require('lspconfig').clangd.setup{
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--pch-storage=memory",
+    "--clang-tidy",
+    "--suggest-missing-includes",
+    "--all-scopes-completion",
+    "--pretty",
+    "--header-insertion=never",
+    "-j=4",
+    "--inlay-hints",
+    "--header-insertion-decorators",
+  },
+  filetypes = {"c", "cpp", "objc", "objcpp"},
+  -- root_dir = utils.root_pattern("compile_commands.json", "compile_flags.txt", ".git")
+  init_option = { fallbackFlags = {  "-std=c++2a"  } }
+}
+
+
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
