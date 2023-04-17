c7c71786ff837c25ae747245f6de37c13f80c62b
ElPatatone <rahman.hm2002@gmail.com>
2023-04-17 17:18:38 +0100

    fix: begin cleaning lua config for lsp

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,27 +1,27 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-    PACKER_BOOTSTRAP = fn.system {
+    PACKER_BOOTSTRAP = fn.system({
         "git",
         "clone",
         "--depth",
         "1",
         "https://github.com/wbthomason/packer.nvim",
         install_path,
-    }
-    print "Installing packer close and reopen Neovim..."
-    vim.cmd [[packadd packer.nvim]]
+    })
+    print("Installing packer close and reopen Neovim...")
+    vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
+vim.cmd([[
 augroup packer_user_config
 autocmd!
 autocmd BufWritePost plugins.lua source <afile> | PackerSync
 augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -30,102 +30,90 @@ if not status_ok then
 end
 
 -- Have packer use a popup window
-packer.init {
+packer.init({
     display = {
         open_fn = function()
-            return require("packer.util").float { border = "rounded" }
+            return require("packer.util").float({ border = "rounded" })
         end,
     },
-}
+})
 
 -- Install your plugins here
 return packer.startup(function(use)
     -- My plugins here
 
-    use "wbthomason/packer.nvim" -- Have packer manage itself
-    use "nvim-lua/popup.nvim"  -- An implementation of the Popup API from vim in Neovim
-    use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+    use("wbthomason/packer.nvim") -- Have packer manage itself
+    use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+    use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 
     --nicer nvim
-    use "lukas-reineke/indent-blankline.nvim"
-    use "nvim-lualine/lualine.nvim"
-    use "akinsho/bufferline.nvim"
-    use "kyazdani42/nvim-tree.lua"
-    use "norcalli/nvim-colorizer.lua"
+    use("lukas-reineke/indent-blankline.nvim")
+    use("nvim-lualine/lualine.nvim")
+    use("akinsho/bufferline.nvim")
+    use("kyazdani42/nvim-tree.lua")
+    use("norcalli/nvim-colorizer.lua")
+    require("colorizer").setup()
+ 
     --editing support
-    use "windwp/nvim-autopairs"
-    use "karb94/neoscroll.nvim"
-    use "kylechui/nvim-surround"
+    use("windwp/nvim-autopairs")
+    use("kylechui/nvim-surround")
+    require("nvim-surround").setup()
+
+
     -- commenting
-    use "numToStr/Comment.nvim"
+    use("numToStr/Comment.nvim")
 
     -- telescope
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.0",
         -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
-    use "nvim-telescope/telescope-media-files.nvim"
+    use("nvim-telescope/telescope-media-files.nvim")
 
-    use "kyazdani42/nvim-web-devicons"
+    use("kyazdani42/nvim-web-devicons")
 
     -- treesitter
-    use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
-    use "JoosepAlviste/nvim-ts-context-commentstring"
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    use("JoosepAlviste/nvim-ts-context-commentstring")
 
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
+    use("theprimeagen/harpoon")
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
 
     --colorschemes
-    use "ellisonleao/gruvbox.nvim"
-    use "bluz71/vim-moonfly-colors"
-    use "bignimbus/pop-punk.vim"
-    use "windwp/nvim-ts-autotag"
-    use "owickstrom/vim-colors-paramount"
-
-    -- lsp
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
-
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
-            {'dsznajder/vscode-es7-javascript-react-snippets'},
-
-            -- Formatter
-            {"jose-elias-alvarez/null-ls.nvim"}
-        }
-    }
-
+    use("ellisonleao/gruvbox.nvim")
+    use("bluz71/vim-moonfly-colors")
+    use("bignimbus/pop-punk.vim")
+    use("windwp/nvim-ts-autotag")
+    use("owickstrom/vim-colors-paramount")
 
     --markdown
     use({
         "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
+        run = function()
+            vim.fn["mkdp#util#install"]()
+        end,
     })
 
     --git
-    use "lewis6991/gitsigns.nvim"
+    use("lewis6991/gitsigns.nvim")
 
-    -- web dev
-    require 'colorizer'.setup()
-    require("nvim-surround").setup()
-    --organisation
+    -- productivity
+    use("nvim-neorg/neorg")
+
+    --Autocompletion
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-path")
+
+    --Snippets
+    use("L3MON4D3/LuaSnip")
+    use("rafamadriz/friendly-snippets")
+    use("dsznajder/vscode-es7-javascript-react-snippets")
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -94,25 +94,10 @@ _G.packer_plugins = {
     path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
-  ["cmp-nvim-lsp"] = {
-    loaded = true,
-    path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
-    url = "https://github.com/hrsh7th/cmp-nvim-lsp"
-  },
-  ["cmp-nvim-lua"] = {
-    loaded = true,
-    path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
-    url = "https://github.com/hrsh7th/cmp-nvim-lua"
-  },
   ["cmp-path"] = {
     loaded = true,
     path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
-  },
-  cmp_luasnip = {
-    loaded = true,
-    path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
-    url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["friendly-snippets"] = {
     loaded = true,
@@ -139,11 +124,6 @@ _G.packer_plugins = {
     path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
-  ["lsp-zero.nvim"] = {
-    loaded = true,
-    path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
-    url = "https://github.com/VonHeikemen/lsp-zero.nvim"
-  },
   ["lualine.nvim"] = {
     loaded = true,
     path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/lualine.nvim",
@@ -154,25 +134,10 @@ _G.packer_plugins = {
     path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/markdown-preview.nvim",
     url = "https://github.com/iamcco/markdown-preview.nvim"
   },
-  ["mason-lspconfig.nvim"] = {
+  neorg = {
     loaded = true,
-    path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
-    url = "https://github.com/williamboman/mason-lspconfig.nvim"
-  },
-  ["mason.nvim"] = {
-    loaded = true,
-    path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/mason.nvim",
-    url = "https://github.com/williamboman/mason.nvim"
-  },
-  ["neoscroll.nvim"] = {
-    loaded = true,
-    path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/neoscroll.nvim",
-    url = "https://github.com/karb94/neoscroll.nvim"
-  },
-  ["null-ls.nvim"] = {
-    loaded = true,
-    path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
-    url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
+    path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/neorg",
+    url = "https://github.com/nvim-neorg/neorg"
   },
   ["nvim-autopairs"] = {
     loaded = true,
@@ -188,11 +153,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
     url = "https://github.com/norcalli/nvim-colorizer.lua"
-  },
-  ["nvim-lspconfig"] = {
-    loaded = true,
-    path = "/home/elpatatone/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
-    url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-surround"] = {
     loaded = true,
