1d212077714520aa99bdf401b91f4cc826dbfec9
ElPatatone <rahman.hm2002@gmail.com>
2023-06-20 22:15:41 +0100

    feat: change the lualine color theme to a simpler one

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -1,8 +1,59 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-  return
+	return
 end
 
-lualine.setup{
-    options = { theme = 'moonfly' }
+local colors = {
+	blue = "#80a0ff",
+	cyan = "#79dac8",
+	black = "#080808",
+	white = "#c6c6c6",
+	red = "#ff5189",
+	violet = "#d183e8",
+	grey = "#303030",
 }
+
+local bubbles_theme = {
+	normal = {
+		a = { fg = colors.black, bg = colors.violet },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.black, bg = colors.black },
+	},
+
+	insert = { a = { fg = colors.black, bg = colors.blue } },
+	visual = { a = { fg = colors.black, bg = colors.cyan } },
+	replace = { a = { fg = colors.black, bg = colors.red } },
+
+	inactive = {
+		a = { fg = colors.white, bg = colors.black },
+		b = { fg = colors.white, bg = colors.black },
+		c = { fg = colors.black, bg = colors.black },
+	},
+}
+
+lualine.setup({
+	options = {
+		-- icons_enabled = true,
+		theme = bubbles_theme,
+		component_separators = "|",
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "filename", "branch" },
+		lualine_c = { "fileformat" },
+		lualine_x = {},
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+
+	inactive_sections = {
+		lualine_a = { "filename" },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = { "location" },
+	},
+	tabline = {},
+	extensions = {},
+})
